9d99282c20503c264d368b4c13b901249599b431
little-pinecone <marta.szymek.mail@gmail.com>
2020-03-09 18:32:09 +0100

    Create a new project.

---
+++ src/main/java/in/keepgrowing/springbootpostgresflyway/SpringBootPostgresFlywayApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootpostgresflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgresFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/keepgrowing/springbootpostgresflyway/SpringBootPostgresFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootpostgresflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
